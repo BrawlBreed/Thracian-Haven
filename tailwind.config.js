b8e90d16afc00c0f801e1743a04461cc00b3db1b
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-06-02 14:34:33 +0300

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,14 @@ module.exports = {
     container: {
       padding: {
         DEFAULT: '1rem',
-        lg: '3rem',
+        lg: '2rem',
       },
     },
     screens: {
       sm: '640px',
       md: '768px',
       lg: '1024px',
-      xl: '1280px',
+      xl: '1234px',
     },
     extend: {
       colors: {
