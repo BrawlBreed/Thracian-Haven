07903dbf6a207abf4e0bbf0f129b765e522fcc6e
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-06-05 20:38:26 +0300

    added

--- src/components/House.js
+++ src/components/House.js
@@ -8,28 +8,28 @@ const House = ({ house }) => {
     <div className='bg-white shadow-lg p-5 rounded-lg rounded-tl-[90px] w-full max-w-[352px] mx-auto'>
       <img className='mb-4' src={house.image} alt='' />
       <div className='text-sm'>{house.type}</div>
-      <div className='text-lg text-primary font-semibold'>{house.address}</div>
-      <div className='text-base font-medium text-secondary mb-4'>
+      <div className='text-lg font-semibold'>{house.address}</div>
+      <div className='text-base font-medium text-violet-700 mb-4'>
         $ {house.price}
       </div>
-      <div className='flex gap-x-5'>
-        <div className='flex items-center gap-x-1'>
-          <div className='text-gray-500 text-2xl'>
+      <div className='flex gap-x-2'>
+        <div className='flex items-center'>
+          <div className='text-gray-500 text-[20px] rounded-full w-8 h-8 flex justify-center items-center'>
             <BiBed />
           </div>
-          <div className='text-gray-600'>{house.bedrooms}</div>
+          <div>{house.bedrooms}</div>
         </div>
-        <div className='flex items-center gap-x-1'>
-          <div className='text-gray-500 text-2xl'>
+        <div className='flex items-center'>
+          <div className='text-gray-500 text-[20px] rounded-full w-8 h-8 flex justify-center items-center'>
             <BiBath />
           </div>
-          <div className='text-gray-600'>{house.bathrooms}</div>
+          <div>{house.bathrooms}</div>
         </div>
-        <div className='flex items-center gap-x-1'>
-          <div className='text-gray-500 text-2xl'>
+        <div className='flex items-center'>
+          <div className='text-gray-500 text-[20px] rounded-full w-8 h-8 flex justify-center items-center'>
             <BiArea />
           </div>
-          <div className='text-gray-600'>{house.surface}</div>
+          <div>{house.surface}</div>
         </div>
       </div>
     </div>
