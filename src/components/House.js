077db864fb011ce6728baac8df6ed307df8f4d46
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-06-05 21:11:53 +0300

    added

--- src/components/House.js
+++ src/components/House.js
@@ -7,30 +7,32 @@ const House = ({ house }) => {
   return (
     <div className='bg-white shadow-lg p-5 rounded-lg rounded-tl-[90px] w-full max-w-[352px] mx-auto'>
       <img className='mb-4' src={house.image} alt='' />
-      <div className='text-sm'>{house.type}</div>
-      <div className='text-lg font-semibold'>{house.address}</div>
-      <div className='text-base font-medium text-violet-700 mb-4'>
-        $ {house.price}
+      <div className='bg-green-500 rounded-full text-base text-white px-3 inline-block mb-2'>
+        {house.type}
       </div>
-      <div className='flex gap-x-2'>
-        <div className='flex items-center'>
-          <div className='text-gray-500 text-[20px] rounded-full w-8 h-8 flex justify-center items-center'>
+      <div className='text-xl font-medium'>{house.address}</div>
+      <div className='flex gap-x-4 my-4'>
+        <div className='flex items-center text-gray-600 gap-1'>
+          <div className='text-[20px] rounded-full'>
             <BiBed />
           </div>
-          <div>{house.bedrooms}</div>
+          <div className='text-base'>{house.bedrooms}</div>
         </div>
-        <div className='flex items-center'>
-          <div className='text-gray-500 text-[20px] rounded-full w-8 h-8 flex justify-center items-center'>
+        <div className='flex items-center text-gray-600 gap-1'>
+          <div className='text-[20px] rounded-full'>
             <BiBath />
           </div>
-          <div>{house.bathrooms}</div>
+          <div className='text-base'>{house.bathrooms}</div>
         </div>
-        <div className='flex items-center'>
-          <div className='text-gray-500 text-[20px] rounded-full w-8 h-8 flex justify-center items-center'>
+        <div className='flex items-center text-gray-600 gap-1'>
+          <div className='text-[20px] rounded-full'>
             <BiArea />
           </div>
-          <div>{house.surface}</div>
+          <div className='text-base'>{house.surface}</div>
         </div>
+      </div>
+      <div className='text-lg font-semibold text-violet-600 mb-4'>
+        $ {house.price}
       </div>
     </div>
   );
