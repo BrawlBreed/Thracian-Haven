701ef60a8c38e05c42970eb8a5563faf4857acd9
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-06-06 19:47:25 +0300

    added

--- src/components/HouseList.js
+++ src/components/HouseList.js
@@ -17,8 +17,8 @@ const HouseList = () => {
       <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8'>
         {houses.map((house, index) => {
           return (
-            <Link to={`/property/${house.id}`}>
-              <House house={house} key={index} />;
+            <Link to={`/property/${house.id}`} key={index}>
+              <House house={house} />
             </Link>
           );
         })}
--- src/pages/PropertyDetails.js
+++ src/pages/PropertyDetails.js
@@ -1,7 +1,44 @@
 import React from 'react';
 
+// import houseData
+import { housesData } from '../data';
+
+// import useParams
+import { useParams } from 'react-router-dom';
+
 const PropertyDetails = () => {
-  return <div>HouseDetails</div>;
+  const { id } = useParams();
+  const property = housesData.find((house) => {
+    return house.id === parseInt(id);
+  });
+
+  return (
+    <div className='container mx-auto'>
+      <div className='flex flex-col gap-6 lg:flex-row'>
+        <div>
+          <img src={property.image} alt='' />
+        </div>
+        <div className='flex-1'>
+          <h2 className='text-2xl font-semibold'>{property.name}</h2>
+          <h3 className='text-lg mb-4'>{property.address}</h3>
+          <div className='mb-2 flex gap-x-2 text-base'>
+            <div className='bg-green-500 rounded-full text-white px-3 inline-block'>
+              {property.type}
+            </div>
+            <div className='bg-violet-500 rounded-full text-white px-3 inline-block'>
+              {property.country}
+            </div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod neque,
+            saepe natus et odio maiores voluptas, accusamus id ex eligendi earum
+            repellat cum labore suscipit quos cupiditate ea explicabo
+            reprehenderit?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PropertyDetails;
