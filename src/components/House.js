7a541d4bb34c00109896c27877a0da6c6b757b5c
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-06-06 23:04:01 +0300

    added

--- src/components/House.js
+++ src/components/House.js
@@ -7,7 +7,7 @@ const House = ({ house }) => {
   return (
     <div className='bg-white shadow-1 p-5 rounded-lg rounded-tl-[90px] w-full max-w-[352px] mx-auto cursor-pointer hover:shadow-2xl transition'>
       <img className='mb-8' src={house.image} alt='' />
-      <div className='mb-2 flex gap-x-2 text-sm'>
+      <div className='mb-4 flex gap-x-2 text-sm'>
         <div className='bg-green-500 rounded-full text-white px-3 inline-block'>
           {house.type}
         </div>
@@ -15,7 +15,7 @@ const House = ({ house }) => {
           {house.country}
         </div>
       </div>
-      <div className='text-xl font-medium'>{house.address}</div>
+      <div className='text-lg font-semibold max-w-[260px]'>{house.address}</div>
       <div className='flex gap-x-4 my-4'>
         <div className='flex items-center text-gray-600 gap-1'>
           <div className='text-[20px] rounded-full'>
