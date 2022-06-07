49d99b221ff8e82ab43e3cd5510f95a9f53c0619
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-06-07 17:44:09 +0300

    added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,7 +7,12 @@ const Banner = () => {
   return (
     <section className='h-full max-h-[640px] mb-52'>
       <div className='flex flex-col lg:flex-row'>
-        <div className='flex-1'>left</div>
+        <div className='lg:ml-[135px] flex items-center flex-1'>
+          <h1 className='text-[58px] font-semibold leading-none'>
+            <span className='text-violet-700'>Rent</span> Your Dream House With
+            Us.
+          </h1>
+        </div>
         <div className='flex-1 lg:flex justify-end items-end'>
           <img src={Image} alt='' />
         </div>
