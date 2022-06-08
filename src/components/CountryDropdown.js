c6a2e14f654bf4fca714f6e768ce5c6ecc48e77b
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-06-08 19:41:12 +0300

    added

--- src/components/CountryDropdown.js
+++ src/components/CountryDropdown.js
@@ -1,45 +1,47 @@
 import React, { useState, useContext } from 'react';
-
 // import icons
 import { RiMapPinLine, RiArrowDownSLine, RiArrowUpSLine } from 'react-icons/ri';
-
 // import context
 import { HouseContext } from './HouseContext';
+// import headless ui components
+import { Menu } from '@headlessui/react';
 
 const CountryDropdown = () => {
   const { country, setCountry, countries } = useContext(HouseContext);
-  const [dropdown, setDropdown] = useState(false);
-
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className='dropdown relative'>
-      <div onClick={() => setDropdown(!dropdown)} className='dropdown-btn'>
+    <Menu as='div' className='dropdown relative'>
+      <Menu.Button
+        onClick={() => setIsOpen(!isOpen)}
+        className='dropdown-btn w-full text-left'
+      >
         <RiMapPinLine className='dropdown-icon-primary' />
         <div>
           <div className='text-[15px] font-medium leading-tight'>{country}</div>
           <div className='text-[13px]'>Select your place</div>
         </div>
-        {dropdown ? (
+        {isOpen ? (
           <RiArrowUpSLine className='dropdown-icon-secondary' />
         ) : (
           <RiArrowDownSLine className='dropdown-icon-secondary' />
         )}
-      </div>
-      {dropdown && (
-        <ul className='dropdown-menu absolute w-full bg-white z-10'>
-          {countries.map((country, index) => {
-            return (
-              <li
-                onClick={() => setCountry(country)}
-                key={index}
-                className='cursor-pointer hover:text-violet-700 transition'
-              >
-                {country}
-              </li>
-            );
-          })}
-        </ul>
-      )}
-    </div>
+      </Menu.Button>
+
+      <Menu.Items className='dropdown-menu absolute w-full bg-white z-10'>
+        {countries.map((country, index) => {
+          return (
+            <Menu.Item
+              as='li'
+              onClick={() => setCountry(country)}
+              key={index}
+              className='cursor-pointer hover:text-violet-700 transition list-none'
+            >
+              {country}
+            </Menu.Item>
+          );
+        })}
+      </Menu.Items>
+    </Menu>
   );
 };
 
